31a5c8978eace8d195f84ebf33c2315033cc6436
tesa par <109278743+tes404@users.noreply.github.com>
2023-02-04 22:41:24 +0530

    Add files via upload
    today is learning part my php html javascript and CSS and react js

---
+++ lll/index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ts</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Sriracha&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css1.css">
+</head>
+<body>
+    <img class="bg" src="bg.jpg" alt="IIT Kharagpur">
+    <div class="container">
+        <h1>We tes</h3>
+        <p>Enter your details and submit this form to confirm your participation in the trip </p>
+        <?php
+        if($insert == true){
+        echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining us for the US trip</p>";
+        }
+    ?>
+        <form action="index.php" method="post">
+            <input type="text" name="name" id="name" placeholder="Enter your name">
+            <input type="text" name="age" id="age" placeholder="Enter your Age">
+            <input type="text" name="gender" id="gender" placeholder="Enter your gender">
+            <input type="email" name="email" id="email" placeholder="Enter your email">
+            <input type="phone" name="phone" id="phone" placeholder="Enter your phone">
+            <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter any other information here"></textarea>
+            <button class="btn">Submit</button> 
+        </form>
+    </div>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php tutorial</title>
+</head>
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.container{
+    max-width: 80%;
+    background-color: rgb(228, 195, 195);
+    margin: auto;
+    padding: 23px;
+}
+</style>
+<body>
+    <div class="container">
+    <h1> Lets learn about PHP </h1>
+    <p>Your party status is here:</p>
+    <?php
+    $age = 6;
+    if($age>18){
+        echo "You can go to the party";
+    }
+    else if($age==7){
+        echo "You are 7 years old";
+    }
+    else if($age==6){
+        echo "You are 6 years old";
+    }
+    else{
+        echo "You can not go to the party";
+    }
+    // Arrays in php
+    $languages = array("Python", "C++", "php", "NodeJs"); 
+    // echo count($languages);   
+    // echo $languages[0];   
+
+    // Loops in PHP
+    $a = 0;
+    while ($a <= 10) {
+        echo "<br>The value of a is: ";
+        echo $a;
+        $a++;
+    }
+
+    // Iterating arrays in PHP using while loop
+    $a = 0;
+    while ($a < count($languages)) {
+        echo "<br>The value of language is: ";
+        echo $languages[$a];
+        $a++;
+    }
+
+    // Do while loop
+    $a = 200;
+    do {
+        echo "<br>The value of a is: ";
+        echo $a;
+        $a++;
+    } while ($a < 10);
+
+    // For loop
+    for ($a=60; $a < 10; $a++) { 
+        echo "<br>The value of a from the for loop is: ";
+        echo $a;
+    }
+
+    foreach ($languages as $value) {
+        echo "<br>The value from foreach loop is ";
+        echo $value;
+    }
+
+    function print5(){
+        echo "FIVE";
+    }
+    print5();
+    print5();
+    print5();
+    print5();
+    function print_number($number){
+        echo "<br>Your number is ";
+        echo $number;
+    }
+    print_number(45);
+    print_number(435);
+    print_number(5);
+    ?>
+
+    </div>
+</body>
+</html>
+
+    <script src="index.js"></script>
+    
+</body>
+</html>
